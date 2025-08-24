e6dae4be2bd5cef499a47c2b806e2e5bd644d693
AammarTufail <m.aammar.tufail@outlook.com>
2025-08-24 22:56:51 +0200

    test django

---
+++ courses/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from courses.models import Courses
+from django.http import HttpResponse
+
+
+# Create your views here.
+def course_detail(request, pk):
+    course = get_object_or_404(Courses, pk=pk)
+    # print(course)
+    # return HttpResponse(course)
+    context = {
+        'course': course,
+    }
+    return render(request, 'course_detail.html', context)
